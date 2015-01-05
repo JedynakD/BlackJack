554f3ab46d379fcb73ac1db476d2b618162c6ba8
User name <you@example.com>
2015-01-05 12:46:27 +0100

    upgraded path and rebuilded app

---
+++ src/jack/ScoreCalculator.java
@@ -0,0 +1,18 @@
+package jack;
+
+public class ScoreCalculator {
+	private int score;
+	private CardDeck playerDeck;
+
+	public ScoreCalculator(CardDeck playerDeck) {
+		this.playerDeck = playerDeck;
+	}
+
+	public int calulateScore() {
+		score = 0;
+		for (Card card : playerDeck.getCardDeck()) {
+			score = score + card.getValue();
+		}
+		return score;
+	}
+}
