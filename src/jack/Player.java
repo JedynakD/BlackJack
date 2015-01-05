6f56d1ce0d3a70c26aecb717e2b058deac543540
JedynakD <djedynak90@gmail.com>
2015-01-05 13:28:02 +0100

    rebuilded app

---
+++ src/jack/Player.java
@@ -0,0 +1,26 @@
+package jack;
+
+public class Player {
+
+	private Card playersCard;
+	private CardDeck playerDeck;
+	private CardDeck originalDeck;
+
+	public Player(CardDeck playerDeck, CardDeck originalDeck) {
+		this.playerDeck = playerDeck;
+		this.originalDeck = originalDeck;
+	}
+
+	public void draw() {
+		playersCard = originalDeck.getOneCardFromDeck();
+		playerDeck.addCardToCardDeck(playersCard);
+	}
+
+	public void pass() {
+		System.out.println("Player passes");
+	}
+	
+	public void showYourCard(){
+		System.out.println(playersCard);
+	}
+}
