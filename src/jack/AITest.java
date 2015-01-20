9bc195dd2e7fb295ecad00cd3b553d44eb26d89f
JedynakD <djedynak90@gmail.com>
2015-01-20 21:58:20 +0100

    added AITest

---
+++ src/jack/AITest.java
@@ -0,0 +1,46 @@
+package jack;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InOrder;
+
+import static org.mockito.Mockito.*;
+
+public class AITest {
+    private Player player;
+    private ScoreCalculator calculator;
+    InOrder inOrder;
+    private AI ai;
+
+    @Before
+    public void beforeAITest() {
+        player = mock(Player.class);
+        calculator = mock(ScoreCalculator.class);
+        ai = new AI(player, calculator);
+        inOrder = inOrder(player, calculator);
+    }
+
+    @Test
+    public void shouldControlPlayerUntilScoreIsEighteen() {
+        when(calculator.calculateScore()).thenReturn(5).thenReturn(10).thenReturn(AI.MAX_SCORE);
+
+        ai.controlPlayer();
+
+        inOrder.verify(player).draw();
+        inOrder.verify(player).showYourCard();
+        inOrder.verify(player).draw();
+        inOrder.verify(player).showYourCard();
+        inOrder.verify(player).pass();
+    }
+
+    @Test
+    public void shouldNotControlPlayerWhenScoreIsMoreThanEighteen() {
+        when(calculator.calculateScore()).thenReturn(20);
+
+        ai.controlPlayer();
+
+        inOrder.verify(player, never()).draw();
+        inOrder.verify(player, never()).showYourCard();
+        inOrder.verify(player, times(1)).pass();
+    }
+}
